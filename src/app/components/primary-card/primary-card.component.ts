3278156695f93451235674dc5aad1f31427c18e2
BeverleyB <beverley.beranger@gmail.com>
2024-03-27 18:45:24 +0100

    Update html and css of primary cards

--- src/app/components/primary-card/primary-card.component.ts
+++ src/app/components/primary-card/primary-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CardsData } from '../../interfaces/cards-data.interface';
 
 @Component({
   selector: 'app-primary-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './primary-card.component.scss'
 })
 export class PrimaryCardComponent {
-
+  @Input() card: CardsData = {title: '', illustrationSrc: '', buttonLabel: '', type: '', id: ''};
 }
