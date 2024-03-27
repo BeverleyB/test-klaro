00082bca5bd6455bd198137cf2f66d0a41ccda90
BeverleyB <beverley.beranger@gmail.com>
2024-03-27 15:42:43 +0100

    Add differents cards components

---
+++ src/app/components/primary-card/primary-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-card',
+  templateUrl: './primary-card.component.html',
+  styleUrl: './primary-card.component.scss'
+})
+export class PrimaryCardComponent {
+
+}
