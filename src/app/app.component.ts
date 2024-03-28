036e5ab41cbbc28c5ec7bcaece32f443380e5ed5
BeverleyB <beverley.beranger@gmail.com>
2024-03-28 17:16:23 +0100

    Create function and template for caroussel in mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CardsData } from './interfaces/cards-data.interface';
 import { ApiService } from './services/api.service';
-import { error } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +10,8 @@ import { error } from 'console';
 export class AppComponent implements OnInit {
   primaryCards: CardsData[] = [];
   secondaryCards: CardsData[] = [];
-  name: string = 'Beverley'
-  title: string = 'klaro'
+  name: string = 'Beverley';
+  title: string = 'klaro';
 
   constructor(private ApiService: ApiService) {}
 
@@ -27,4 +26,22 @@ export class AppComponent implements OnInit {
     });
   }
 
+  slide(slideTo: string) {
+    const slideDiv = document.getElementById("container-secondary-cards");
+    const btnScrollLeft = document.getElementById("scroll-left");
+    const btnScrollRight = document.getElementById("scroll-right");
+
+    if (slideDiv && btnScrollLeft && btnScrollRight) {
+
+      if (slideTo === 'right') {
+        slideDiv.scrollLeft += slideDiv.scrollWidth;
+        btnScrollLeft.style.display = 'block';
+        btnScrollRight.style.display = 'none';
+      } else {
+        slideDiv.scrollLeft -= slideDiv.scrollWidth;
+        btnScrollLeft.style.display = 'none';
+        btnScrollRight.style.display = 'block';
+      }
+    }
+  }
 }
